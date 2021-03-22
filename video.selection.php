cc76fdd151b86cb821c13e94936f7b6449175ace
NitLyn <81189738+NitLyn@users.noreply.github.com>
2021-03-22 15:48:33 -0400

    Add files via upload

---
+++ video.selection.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/styleVideoSelection.css">
+    <script type="text/javascript" src="./JS/show.home.video.js"></script>
+    <script defer src="./JS/cssChange.js"></script>
+    <title>Document</title>
+</head>
+<body class="dark">
+    <div id="video.container">
+    <div id="eqao1">
+
+        
+    </div>
+    </div>
+</body>
+</html>
