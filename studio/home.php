544c8a068afc8ce2b733a1aa38feeb110b08f0e0
NitLyn <81189738+NitLyn@users.noreply.github.com>
2021-03-24 09:44:54 -0400

    Add files via upload

---
+++ studio/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="../CSS/styleStudio.css">
+    <link rel="shortcut icon" type="image/png" href="./icons/favicon.png">
+    <script defer src="./JS/cssChange.js"></script>
+    <title>Nitlyn | Production Center</title>
+</head>
+<body class="dark">
+    <div id="body">
+        <?php
+        include "modal.php"
+        ?>
+    </div>
+    <div id="footer">
+        <?php
+        include "./footer.php";
+        ?>
+    </div>
+</body>
+</html>
---
+++ studio/modal.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Modal</title>
+  <link rel="stylesheet" href="../CSS/styleModal.css">
+  <link rel="stylesheet" href="../CSS/style.css">
+    <script defer src="../JS/upldPrmt.js"></script>
+    <script defer src="../JS/cssChange.js"></script>
+</head>
+<body class="dark">
+  <button data-modal-target="#modal" class="upl-bttn">Upload</button>
+  <div class="modal" id="modal">
+    <div class="modal-header">
+      <div class="title">Upload</div>
+      <button data-close-button class="close-button">&times;</button>
+    </div>
+    <div class="modal-body">
+    <?php
+    include "./ddupl.html";
+    ?>
+    </div>
+  </div>
+  <div id="overlay"></div>
+</body>
+</html>
+
+
+
+
